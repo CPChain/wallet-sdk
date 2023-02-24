9a88f3c88eda103cde3613c79f12608ee8bc1717
zhangkai-cpchain <zhangk@cpchain.io>
2023-02-24 14:08:47 +0800

    add index.d.ts

--- src/realm-extends/index.ts
+++ src/realm-extends/index.ts
@@ -1,2 +1,8 @@
+/**
+ *  类属性装饰器，把enum转换为realm数组库可识别的存储类型
+ */
 export function RealmEnum(..._args: any) {}
+/**
+ *  类属性装饰器，把enum转换为realm数组库可识别的存储类型
+ */
 export function RealmBigNumber(..._args: any) {}
