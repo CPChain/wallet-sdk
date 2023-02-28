69126a1b9d6e1395f981231e3f1824a606f743f5
zhangkai-cpchain <zhangk@cpchain.io>
2023-02-28 14:10:31 +0800

    add json type

--- src/realm-extends/index.ts
+++ src/realm-extends/index.ts
@@ -1,8 +1,12 @@
 /**
- *  类属性装饰器，把enum转换为realm数组库可识别的存储类型
+ *  类属性装饰器，把enum转换为realm可识别的存储类型
  */
 export function RealmEnum(..._args: any) {}
 /**
- *  类属性装饰器，把BigNumber转换为realm数组库可识别的存储类型
+ *  类属性装饰器，把BigNumber转换为realm可识别的存储类型
  */
 export function RealmBigNumber(..._args: any) {}
+/**
+ *  类属性装饰器，把Object转换为realm数组库可识别的存储类型
+ */
+export function RealmJson(..._args: any) {}
